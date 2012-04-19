89b2982de32861258bdc354d92c87eece7398a6e
Christopher Deutsch <cd@cdeutsch.com>
2012-04-19 11:16:59 -0500

    Got simple Universal app working.

---
+++ MT.JsBridge/MT_JsBridgeViewController.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Drawing;
+
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace MT.JsBridge
+{
+	public partial class MT_JsBridgeViewController : UIViewController
+	{
+		public UIWebView WebView { 
+			get {
+				return webView;
+			}
+		}
+		
+		static bool UserInterfaceIdiomIsPhone {
+			get { return UIDevice.CurrentDevice.UserInterfaceIdiom == UIUserInterfaceIdiom.Phone; }
+		}
+
+		public MT_JsBridgeViewController ()
+			: base (UserInterfaceIdiomIsPhone ? "MT_JsBridgeViewController_iPhone" : "MT_JsBridgeViewController_iPad", null)
+		{
+		}
+		
+		public override void DidReceiveMemoryWarning ()
+		{
+			// Releases the view if it doesn't have a superview.
+			base.DidReceiveMemoryWarning ();
+			
+			// Release any cached data, images, etc that aren't in use.
+		}
+		
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
+		
+		public override void ViewDidUnload ()
+		{
+			base.ViewDidUnload ();
+			
+			// Clear any references to subviews of the main view in order to
+			// allow the Garbage Collector to collect them sooner.
+			//
+			// e.g. myOutlet.Dispose (); myOutlet = null;
+			
+			ReleaseDesignerOutlets ();
+		}
+		
+		public override bool ShouldAutorotateToInterfaceOrientation (UIInterfaceOrientation toInterfaceOrientation)
+		{
+			// Return true for supported orientations
+			if (UserInterfaceIdiomIsPhone) {
+				return (toInterfaceOrientation != UIInterfaceOrientation.PortraitUpsideDown);
+			} else {
+				return true;
+			}
+		}
+	}
+}
+
